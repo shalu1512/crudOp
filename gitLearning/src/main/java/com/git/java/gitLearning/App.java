5f6038d3a0243dba8db0e1c02c89b815dcd6199f
shalu1512 <@Shalupatel#123>
2023-06-07 19:42:02 +0530

    initial commit

---
+++ gitLearning/src/main/java/com/git/java/gitLearning/App.java
@@ -0,0 +1,13 @@
+package com.git.java.gitLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "STS tool git learning!" );
+    }
+}
